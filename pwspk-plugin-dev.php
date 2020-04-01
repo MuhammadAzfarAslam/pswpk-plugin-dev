c1a23d4cdda29dbc40cb8fc9313a19d31ee3b108
Muhammad Azfar Aslam <azfarnaslam06@gmail.com>
2020-04-01 23:03:31 +0500

    Adding css & js files + adding shortcode file in include directory

--- pwspk-plugin-dev.php
+++ pwspk-plugin-dev.php
@@ -12,15 +12,15 @@
 
 defined('ABSPATH') || die("You cant access this file path");
 
-add_action('init', 'pwspk_init');
+include plugin_dir_path(__FILE__)."inc/shortcodes.php";
 
-function pwspk_init(){
-    add_shortcode('test', 'pwspk_my_shortcode');
+//add_filter('the_title', 'pwspk_the_title');
+function pwspk_the_title($title){
+    return "<strong>{$title}</strong>";
 }
 
-function pwspk_my_shortcode($atts){
-    $atts = shortcode_atts(array(
-        'message' => 'Hello World',
-    ), $atts, 'test');
-    return $atts['message'];
+add_action('wp_enqueue_scripts', 'pwspk_wp_enqueue_scripts');
+function pwspk_wp_enqueue_scripts(){
+    wp_enqueue_style('pwspk_dev_plugin', plugin_dir_url(__FILE__)."assets/css/style.css");
+    wp_enqueue_script('pwspk_dev_script', plugin_dir_url(__FILE__)."assets/js/custom.js", array(), '1.0.0', true);
 }
