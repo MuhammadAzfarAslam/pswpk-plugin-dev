b43ac249c147b2285b4e4ad660621d928db4559b
Muhammad Azfar Aslam <azfarnaslam06@gmail.com>
2020-04-02 01:13:05 +0500

    Adding menu and sub-menu in dashboard

--- pwspk-plugin-dev.php
+++ pwspk-plugin-dev.php
@@ -24,3 +24,19 @@ function pwspk_wp_enqueue_scripts(){
     wp_enqueue_style('pwspk_dev_plugin', plugin_dir_url(__FILE__)."assets/css/style.css");
     wp_enqueue_script('pwspk_dev_script', plugin_dir_url(__FILE__)."assets/js/custom.js", array(), '1.0.0', true);
 }
+
+add_action('admin_menu', 'pwspk_plugin_menu');
+function pwspk_plugin_menu(){
+    add_menu_page('PWSPK Options', 'PWSPK Options', 'manage_options', 'pwspk-options', 'pwspk_options_func', $icon_url, $position);
+    add_submenu_page('pwspk-options', 'PWSPK Settings', 'PWSPK Settings', 'manage_options', 'pwspk-settings', 'pwspk_settings_func');
+    add_submenu_page('pwspk-options', 'PWSPK Layout', 'PWSPK Layout', 'manage_options', 'pwspk-layout', 'pwspk_layout_func');
+}
+function pwspk_options_func(){
+    echo "<h1> PWSPK Options Menu</h1>";
+}
+function pwspk_settings_func(){
+    echo "<h1> PWSPK Settings Menu</h1>";
+}
+function pwspk_layout_func(){
+    echo "<h1> PWSPK Layout Menu</h1>";
+}
