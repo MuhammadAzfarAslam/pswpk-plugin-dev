ed514801a60a5c2366066e684dde938f65f6e290
Muhammad Azfar Aslam <azfarnaslam06@gmail.com>
2020-04-02 02:04:35 +0500

    Add a form in options menu and saving+retrieving data in options table

--- pwspk-plugin-dev.php
+++ pwspk-plugin-dev.php
@@ -26,13 +26,35 @@ function pwspk_wp_enqueue_scripts(){
 }
 
 add_action('admin_menu', 'pwspk_plugin_menu');
+add_action('admin_menu','pwspk_process_form_settings');
 function pwspk_plugin_menu(){
     add_menu_page('PWSPK Options', 'PWSPK Options', 'manage_options', 'pwspk-options', 'pwspk_options_func', $icon_url, $position);
     add_submenu_page('pwspk-options', 'PWSPK Settings', 'PWSPK Settings', 'manage_options', 'pwspk-settings', 'pwspk_settings_func');
     add_submenu_page('pwspk-options', 'PWSPK Layout', 'PWSPK Layout', 'manage_options', 'pwspk-layout', 'pwspk_layout_func');
 }
-function pwspk_options_func(){
-    echo "<h1> PWSPK Options Menu</h1>";
+register_activation_hook(__FILE__, function(){
+    add_option('pwspk_option_1', '');
+});
+register_deactivation_hook(__FILE__, function(){
+    delete_option('pwspk_option_1');
+});
+function pwspk_process_form_settings(){
+    register_setting('pwspk_option_group', 'pwspk_option_name');
+    if(isset($_POST['action']) && current_user_can('manage_options')){
+        update_option('pwspk_option_1', sanitize_text_field($_POST['pwspk_option_1']));
+    }
+}
+function pwspk_options_func(){ ?>
+    <div class="wrap">
+        <h1> PWSPK Options Menu</h1>
+        <?php settings_errors(); ?>
+        <form action="options.php" method="post">
+            <?php settings_fields('pwspk_option_group'); ?>
+            <label for="">Settings One: <input type="text" name="pwspk_option_1" value="<?php echo esc_html(get_option('pwspk_option_1')); ?>"></label>
+            <?php submit_button('Save Changes'); ?>
+        </form>
+    </div>
+<?php
 }
 function pwspk_settings_func(){
     echo "<h1> PWSPK Settings Menu</h1>";
